2e45d52d54ebe4afcd93cd8e05d376f851f3d812
sowmya_a <amminenisowmyasai@gmail.com>
2022-06-03 10:11:47 +0530

    check if number is odd or even

---
+++ javascript/check if a number is odd or even.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Even or Odd number</title>
+</head>
+<body>
+<script>
+    var no = parseInt(prompt("Enter the number", ""));
+
+    if(no%2 == 0)
+        document.write("The number is Even.");
+    else 
+        document.write("The number is Odd.");
+</script>
+</body>
+</html>

c6ff7a49db04e9eac2705e2a4d00f039891dc5c5
sowmya_a <amminenisowmyasai@gmail.com>
2022-06-04 09:18:04 +0530

    find a armstrong number with an interval[1-100]

---
+++ javascript/find a armstrong number with an interval[1-100].js extention.js
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+        <title>document</title>
+</head>
+<body>
+
+<script>
+    for(var i=0;i<=100;i++){
+    }
+    var num=i;
+    var sum=0;
+    var check number;
+    var armstrong=0;
+    var len=num.to strong().length;
+    var power=0;
+    while(num!=0){
+        armstrong=num%10;
+        power=math.pow(armstrong,len);
+        sum=sum+power;
+        num=parselent(num/10);
+    }
+        
+if(check==sum){
+    document.write(check+"is armstrong number"+"<br>");
+}
+</script>
+</body>
+</html>
